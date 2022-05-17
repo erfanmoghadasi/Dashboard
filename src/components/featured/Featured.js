f90e0654f63ad6c0bf0e212ba29d84ac2711e761
Erfan Moghadasi <erfan.moqadasii@gmail.com>
2022-05-17 12:33:21 +0430

    fix some css bugs

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -27,21 +27,21 @@ function Featured() {
                 <div className="summary">
                     <div className="item">
                         <div className="itemTitle">Target</div>
-                        <div className="itemResult">
+                        <div className="itemResult positive">
                         <KeyboardArrowUpIcon fontSize='small'/>
                             <div className="resultAmount">$12.4k</div>
                         </div>
                     </div>
                     <div className="item">
                         <div className="itemTitle">Last Week</div>
-                        <div className="itemResult">
+                        <div className="itemResult negative">
                         <KeyboardArrowDownIcon fontSize='small'/>
                             <div className="resultAmount">$12.4k</div>
                         </div>
                     </div>
                     <div className="item">
                         <div className="itemTitle">Last Month</div>
-                        <div className="itemResult">
+                        <div className="itemResult negative">
                         <KeyboardArrowDownIcon fontSize='small'/>
                             <div className="resultAmount">$12.4k</div>
                         </div>
